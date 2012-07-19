50374ca275cdc5faf02dafa902d7cf221c53efa0
schegi <schegi@uni-koblenz.de>
2012-07-19 15:39:46 +0200

    Blueprint default implementation structure  for Edge/Graph/Vertex added

---
+++ src/test/java/de/uniko/west/dgds/bpimpls/BpEdge.java
@@ -0,0 +1,46 @@
+package de.uniko.west.dgds.bpimpls;
+
+import java.util.Set;
+
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Vertex;
+
+public class BpEdge implements Edge {
+
+	public Object getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getProperty(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<String> getPropertyKeys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object removeProperty(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setProperty(String arg0, Object arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Vertex getVertex(Direction arg0) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/de/uniko/west/dgds/bpimpls/BpVertex.java
@@ -0,0 +1,52 @@
+package de.uniko.west.dgds.bpimpls;
+
+import java.util.Set;
+
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Query;
+import com.tinkerpop.blueprints.Vertex;
+
+public class BpVertex implements Vertex {
+
+	public Object getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getProperty(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<String> getPropertyKeys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object removeProperty(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setProperty(String arg0, Object arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Iterable<Edge> getEdges(Direction arg0, String... arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Iterable<Vertex> getVertices(Direction arg0, String... arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Query query() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
